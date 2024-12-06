d7fa90398a57238283cd0893a9dbb2cd12f1cd36
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-12-06 20:12:16 -0300

    Finaliza exercicio

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+Console.WriteLine("Doador de sangue");
+
+Console.WriteLine("Digite sua idade");
+int idade = Convert.ToInt32(Console.ReadLine());
+
+if (idade >= 18 && idade  <= 67)
+{
+Console.WriteLine("Pode doar sangue");
+}
+ else {
+Console.WriteLine("Não pode doar sangue");
+ }
